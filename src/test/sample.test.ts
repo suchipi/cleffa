04d30bcd79a3c9d81d62b0f8dffc61a87f5544b7
Lily Skye <git@suchipi.tv>
2025-11-18 11:04:35 -0700

    npm audit fix

--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -14,10 +14,15 @@ test("sample", async () => {
       "code": 1,
       "error": false,
       "stderr": "Error: oh no
+
+    ./src/test/fixtures/sample.ts:17:11
+
+
       at main (<rootDir>/src/test/fixtures/sample.ts:17:11)
       at cleffa (<rootDir>/dist/index.js:105:15)
-      at Object.<anonymous> (<rootDir>/dist/index.js:108:1)
-      at node:internal/main/run_main_module:23:47
+      at runMain (<rootDir>/node_modules/@suchipi/run-main/dist/index.js:28:24)
+      at Object.<anonymous> (<rootDir>/dist/index.js:108:24)
+      at node:internal/main/run_main_module:28:49
     ",
       "stdout": "{ options: { blah: 45 }, args: [ 'yes' ] }
     ",
